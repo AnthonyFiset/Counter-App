ce6da49233a5bdcb9dce8803fc5acc074b1cb739
Anthony Fiset <tonyfise@my.yorku.ca>
2022-01-01 03:45:59 -0500

    Finished app!

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+let CountEl = document.getElementById("counter")
 let count = 0
+let saveEl = document.getElementById("saveEl")
 
 function increment() {
-    count = count + 1
-    document.getElementById("counter").innerText = count
+    count += 1
+    CountEl.innerText = count
 }
 
+function save() {
+    let info = count + " - "
+    saveEl.textContent += info
+    count = 0
+    CountEl.innerText = 0
+}
 
 
